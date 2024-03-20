9de3880404fdcd9c3e4857c25aa0b96e43bd5f3c
MajorTomMan <765719516@qq.com>
2024-03-20 18:49:36 +0800

    增加邮件发送功能

---
+++ spider/src/main/java/com/javbus/spider/spider/utils/EmailUtil.java
@@ -0,0 +1,57 @@
+package com.javbus.spider.spider.utils;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Component
+public class EmailUtil {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+    public void sendEmail(String subject, String body) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(mailUsername);
+            helper.setTo(mailUsername);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(helper.getMimeMessage());
+    }
+    public void sendEmail(String subject, Exception error) {
+        String body = getStackTraceAsString(error);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(mailUsername);
+            helper.setTo(mailUsername);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(helper.getMimeMessage());
+    }
+        // 辅助方法：将异常堆栈信息转换为字符串
+    private String getStackTraceAsString(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
+    }
+}
--- spider/src/test/java/com/javbus/spider/spider/SpiderApplicationTests.java
+++ spider/src/test/java/com/javbus/spider/spider/SpiderApplicationTests.java
@@ -1,12 +1,18 @@
 package com.javbus.spider.spider;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.javbus.spider.spider.utils.EmailUtil;
 
 @SpringBootTest
 class SpiderApplicationTests {
+	@Autowired
+	private EmailUtil util;
 	@Test
-	void contextLoads() {
+	void testMail() {
+		util.sendEmail("测试", "测试");
 	}
 
 }
