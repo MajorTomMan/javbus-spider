ae0a36ff83c5710551dbf0130eadbd83abad8f0c
765719516@qq.com <ubuntu@ip-172-26-4-174.ap-northeast-1.compute.internal>
2025-03-10 11:25:14 +0800

    修复错误

--- javbus/settings.py
+++ javbus/settings.py
@@ -116,7 +116,7 @@ SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 # 使用 FIFO 队列
 SCHEDULER_QUEUE_CLASS = "scrapy_redis.queue.FifoQueue"
 
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "default"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 # 保证去重数据保存在 Redis 中，支持分布式
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
 # 持久化爬取队列（爬虫结束后，保留队列）
