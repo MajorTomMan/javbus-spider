ac8b05d11aee7bb4479e8068535ddf6d93bab42a
MajorTomMan <765719516@qq.com>
2025-03-19 10:39:38 +0800

    启动脚本增加监听oom的代码

--- start.sh
+++ start.sh
@@ -66,20 +66,20 @@ pip install -r "$SCRIPT_DIR/requirements.txt"
 mkdir -p "$SCRIPT_DIR/logs"
 #mkdir -p "$SCRIPT_DIR/outputs"
 
-nohup scrapy crawl index -a is_censored=True > /dev/null 2>&1 &
-PID_index_censored=$!
-echo "Index Censored spider is running with PID: $PID_index_censored"
+#nohup scrapy crawl index -a is_censored=True > /dev/null 2>&1 &
+#PID_index_censored=$!
+#echo "Index Censored spider is running with PID: $PID_index_censored"
 
-nohup scrapy crawl index -a is_censored=False > /dev/null 2>&1 &
-PID_index_uncensored=$!
-echo "Index Uncensored spider is running with PID: $PID_index_uncensored"
+#nohup scrapy crawl index -a is_censored=False > /dev/null 2>&1 &
+#PID_index_uncensored=$!
+#echo "Index Uncensored spider is running with PID: $PID_index_uncensored"
 
-nohup scrapy crawl actresses_list -a is_censored=True > /dev/null  2>&1 &
-PID_actresses_list_censored=$!
-echo "Genre Actresses List Censored spider is running with PID: $PID_actresses_list_censored"
-nohup scrapy crawl actresses_list -a is_censored=False > /dev/null 2>&1 &
-PID_actresses_list_uncensored=$!
-echo "Genre Actresses List Uncensored spider is running with PID: $PID_actresses_list_uncensored"
+#nohup scrapy crawl actresses_list -a is_censored=True > /dev/null  2>&1 &
+#PID_actresses_list_censored=$!
+#echo "Genre Actresses List Censored spider is running with PID: $PID_actresses_list_censored"
+#nohup scrapy crawl actresses_list -a is_censored=False > /dev/null 2>&1 &
+#PID_actresses_list_uncensored=$!
+#echo "Genre Actresses List Uncensored spider is running with PID: $PID_actresses_list_uncensored"
 nohup scrapy crawl movie > /dev/null 2>&1 &
 PID_movie=$!
 echo "Movie spider is running with PID: $PID_movie"
@@ -92,15 +92,16 @@ nohup scrapy crawl actress_detail > /dev/null 2>&1 &
 PID_actress_detail=$!
 echo "Actress Detail spider is running with PID: $PID_actress_detail"
 
-nohup scrapy crawl genre -a is_censored=True > /dev/null 2>&1 &
-PID_genre_censored=$!
-echo "Genre Censored spider is running with PID: $PID_genre_censored"
+#nohup scrapy crawl genre -a is_censored=True > /dev/null 2>&1 &
+#PID_genre_censored=$!
+#echo "Genre Censored spider is running with PID: $PID_genre_censored"
 
-nohup scrapy crawl genre -a is_censored=False > /dev/null 2>&1 &
-PID_genre_uncensored=$!
-echo "Genre Uncensored spider is running with PID: $PID_genre_uncensored"
+#nohup scrapy crawl genre -a is_censored=False > /dev/null 2>&1 &
+#PID_genre_uncensored=$!
+#echo "Genre Uncensored spider is running with PID: $PID_genre_uncensored"
 
-kill_command_batch=$(echo "$PID_actress_detail $PID_movie $PID_actress_movie $PID_index_censored $PID_index_uncensored $PID_actresses_list_censored $PID_actresses_list_uncensored $PID_genre_censored $PID_genre_uncensored" | xargs -I {} echo "kill -9 {}")
+kill_command_batch=$(echo "$PID_actress_detail $PID_movie $PID_actress_movie " | xargs -I {} echo "kill -9 {}")
+#$PID_index_censored $PID_index_uncensored $PID_actresses_list_censored $PID_actresses_list_uncensored $PID_genre_censored $PID_genre_uncensored
  
 echo "以下是可以用来杀死爬虫进程的命令："
 echo "$kill_command_batch"
